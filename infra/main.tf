92452a5b5037869a2afca8db768022bec8122b9d
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-08-28 23:27:14 +0100

    remove comments

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,6 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc_default" {
-  # id         = "vpc-8afcfce3"
   cidr_block = "172.31.0.0/16"
   tags = {
     "Name" = "Training Budget VPC 1"
@@ -14,7 +13,6 @@ resource "aws_vpc" "vpc_default" {
 }
 
 resource "aws_subnet" "default_subnet" {
-  #id         = "subnet-dd1be7b5"
   cidr_block              = "172.31.0.0/20"
   vpc_id                  = aws_vpc.vpc_default.id
   map_public_ip_on_launch = true
@@ -25,21 +23,3 @@ resource "aws_subnet" "default_subnet" {
     prevent_destroy = true
   }
 }
-
-# data "aws_subnet_ids" "default_subnet_ids" {
-#   vpc_id = aws_vpc.vpc_default.id
-# }
-#
-# data "aws_subnet" "default_subnet" {
-#   id = tolist(data.aws_subnet_ids.default_subnet_ids.ids)[0]
-# }
-#
-# output "default_subnet_id" {
-#   value = data.aws_subnet.default_subnet.id
-# }
-#
-# resource "aws_subnet" "subnet_default" {
-#   vpc_id     = aws_vpc.vpc_default.id
-#   id         = data.aws_subnet.default_subnet.id
-#   cidr_block = "172.31.0.0/20"
-# }
