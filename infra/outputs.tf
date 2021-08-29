43ee3df261aa648b9d286a78c8c4e3116f592c1a
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-08-29 17:47:21 +0100

    rename output > outputs dir

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,8 @@
 output "vm_1_public_ip" {
-  value = aws_instance.env-01-vm-bas.public_ip
+  value = aws_instance.ec2-vm-1.public_ip
+}
 
-
-resource "local_file" "bastion_public_ip_output" {
-  content  = aws_instance.env-01-vm-bas.public_ip
-  filename = "output_bastion_public_ip.txt"
+resource "local_file" "vm_1_public_ip" {
+  content  = aws_instance.ec2-vm-1.public_ip
+  filename = "outputs/vm_1_public_ip.txt"
 }
