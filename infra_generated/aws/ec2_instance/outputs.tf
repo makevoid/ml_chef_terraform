be81f96cb25c94d0c5c1a65533e9a8c80739df2a
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-08-29 05:53:13 +0100

    drop terraformer generated infra

--- infra_generated/aws/ec2_instance/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_instance_ec2-vm-1_id" {
-  value = "aws_instance.ec2-vm-1.id"
-}
--- infra_generated/aws/eni/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_network_interface_tfer--eni-002D-0cefa9ffb217d880c_id" {
-  value = "aws_network_interface.tfer--eni-002D-0cefa9ffb217d880c.id"
-}
