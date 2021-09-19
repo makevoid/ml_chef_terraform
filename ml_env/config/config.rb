0253928d6be06a0612ec80b71e443f6c478d1b08
Francesco Canessa (makevoid) <makevoid@gmail.com>
2021-09-19 20:14:21 +0100

    fluidstack user

--- ml_env/config/config.rb
+++ ml_env/config/config.rb
@@ -2,11 +2,11 @@ PATH = File.expand_path "../../", __FILE__
 
 CHEF_VERSION = "21.8.555" # workstation version # chef version: "17.3.48"
 
-USER = "ubuntu" # using the Ubuntu 18 AWS ML AMI
-# USER = "user" # fluidstack default user
+# USER = "ubuntu" # using the Ubuntu 18 AWS ML AMI
+USER = "user" # fluidstack default user
 
-DEFAULT_IP = nil
-# DEFAULT_IP = "207.53.234.147"
+# DEFAULT_IP = nil
+DEFAULT_IP = "207.53.234.147"
 
 def read_host_ip
   return DEFAULT_IP if DEFAULT_IP
